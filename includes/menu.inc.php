a60da463d1406f62432e90f3aafcfeac0d362308
Gergő <mogema2002@gmail.com>
2021-12-05 07:22:43 +0100

    Create menu.inc.php

---
+++ includes/menu.inc.php
@@ -0,0 +1,21 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <?php
+        foreach($menu as $key => $value) {
+            $active = '';
+            if($_SERVER['REQUEST_URI'] == '/konyvtar/'.$key) $active = ' active';
+            if($key == 'felhasznalo') $key.='&action='.$action;
+            ?>
+            <li class="nav-item<?php echo $active; ?>">
+                <a class="nav-link" href="index.php?page=<?php echo $key; ?>"><?php echo $value; ?></a>
+            </li>
+            <?php
+        }
+      ?>
+    </ul>
+  </div>
+</nav>
