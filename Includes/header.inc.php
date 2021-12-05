83891f463a8d6e455fca6a43e45eefb7c65bd85e
Gergő <mogema2002@gmail.com>
2021-12-05 07:29:56 +0100

    style.css

--- Includes/header.inc.php
+++ Includes/header.inc.php
@@ -2,6 +2,7 @@
 <html lang="hu">
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $title; ?></title>	
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
