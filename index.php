f6331d7a40fbbd97aeeebf48e7f5ce4769bc441d
Gergő <mogema2002@gmail.com>
2022-01-23 09:11:05 +0100

    register check fix

--- index.php
+++ index.php
@@ -44,12 +44,11 @@
         $_SESSION['indexPage']=1;
     }
 
-    if(isset($_REQUEST['register']) and isset( $_SESSION['register']) and $_SESSION['register']==false) {
+    if(isset($_REQUEST['register']) and $_REQUEST['register']==true) {
         $_SESSION['register']=true;
     }elseif(!isset($_REQUEST['register']) and isset( $_SESSION['register']) and $_SESSION['register']==true) {
         $_SESSION['register']=false;
     }
-
     $menu = array('index' => "Főoldal", 
                   'userProfile' => "Felhasználói profil",
                   'userControl' => $szoveg,
