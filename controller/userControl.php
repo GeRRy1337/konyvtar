80ddb53fdcac45d91b5db7f991f06ceda182c4c3
Gergő <mogema2002@gmail.com>
2022-01-23 10:02:34 +0100

    register controll

--- controller/userControl.php
+++ controller/userControl.php
@@ -2,27 +2,40 @@
 
 if(isset($_POST['user']) and isset($_POST['pw'])) {
 	$loginError = '';
-	if(strlen($_POST['user']) == 0) $loginError .= "Nem írtál be felhasználónevet<br>";
-	if(strlen($_POST['pw']) == 0) $loginError .= "Nem írtál be jelszót<br>";
-	if($loginError == '') {
+	if(isset($_POST['pw2']) and isset($_POST['email'])){
 		$sql = "SELECT id FROM users WHERE username = '".$_POST['user']."' ";
 
 		if(!$result = $conn->query($sql)) echo $conn->error;
 
 		if ($result->num_rows > 0) {
+			$loginError .= "Ez a felhasználónév már foglalt<br>";
+		}else{
 			
-			if($row = $result->fetch_assoc()) {
-				$user->set_user($row['id'], $conn);
-				if(md5($_POST['pw']) == $user->get_password()) {
-					$_SESSION["id"] = $row['id'];
-					$_SESSION["username"] = $user->get_username();
-                    header('Location: index.php?page=index');
-                    exit();
-				}
-				else $loginError .= 'Érvénytelen jelszó<br>';
-			}
+			header('Location: index.php?page=userControl');
 		}
-		else $loginError .= 'Érvénytelen felhasználónév<br>';
+	}else{
+		if(strlen($_POST['user']) == 0) $loginError .= "Nem írtál be felhasználónevet<br>";
+		if(strlen($_POST['pw']) == 0) $loginError .= "Nem írtál be jelszót<br>";
+		if($loginError == '') {
+			$sql = "SELECT id FROM users WHERE username = '".$_POST['user']."' ";
+
+			if(!$result = $conn->query($sql)) echo $conn->error;
+
+			if ($result->num_rows > 0) {
+				
+				if($row = $result->fetch_assoc()) {
+					$user->set_user($row['id'], $conn);
+					if(md5($_POST['pw']) == $user->get_password()) {
+						$_SESSION["id"] = $row['id'];
+						$_SESSION["username"] = $user->get_username();
+						header('Location: index.php?page=index');
+						exit();
+					}
+					else $loginError .= 'Érvénytelen jelszó<br>';
+				}
+			}
+			else $loginError .= 'Érvénytelen felhasználónév<br>';
+		}
 	}
 }
 
