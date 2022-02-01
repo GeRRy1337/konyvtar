614f21366da1ac9964df9a5b79c3514d0f926038
Gergő <mogema2002@gmail.com>
2022-02-01 18:09:38 +0100

    favourite controller

--- controller/bookInfo.php
+++ controller/bookInfo.php
@@ -1,7 +1,14 @@
-<?php 
+<?php
     if(!empty($_GET['bookId'])){
         $BookVar->set_book($_GET['bookId'],$conn);
         $author->set_author(1,$conn);
+        if(isset($_POST['favButtonOn'])){
+            $BookVar->delFav($conn,$_SESSION['id']);
+            unset($_POST['favButtonOn']);
+        }elseif(isset($_POST['favButtonOff'])){
+            $BookVar->setFav($conn,$_SESSION['id']);
+            unset($_POST['favButtonOff']);
+        }
         include 'view/bookInfo.php';
     }else{
         header('Location:index.php');
