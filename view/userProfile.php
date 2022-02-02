5d23969b2d0ca9a653eaf4f6149a0b610f0d14f5
Gergő <mogema2002@gmail.com>
2022-02-02 18:07:38 +0100

    user favourite books

--- view/userProfile.php
+++ view/userProfile.php
@@ -45,4 +45,17 @@
     <?php
     }
     ?>
+    <div class="row justify-content-center">
+        <?php
+            if ($favBooks) {
+                foreach($favBooks as $bookId) {
+                    $BookVar->set_book($bookId,$conn);
+                    echo ' <span class="bookItem"><a href="index.php?page=bookInfo&bookId='.$bookId.'"><img src='.$BookVar->get_ImageUrlL().'></a><br>'. $BookVar->get_BookTitle() .' ('.$BookVar->get_YearOfPublication().')' .'</span>';
+                }
+            }
+            else {
+                echo "Nincsenek kedvenc könyveid!";
+            }
+        ?>
+    </div>
 </div>
