9a26a38d9050c4e8bf5b223d584495d748e4ea65
GeRRy1337 <mogema2002@gmail.com>
2022-02-10 09:47:36 +0100

    db actions

--- api/index.php
+++ api/index.php
@@ -2,8 +2,15 @@
     $browser = get_browser(null, true);
     if ($browser["browser_name_pattern"]!="*java*") header('location:/konyvtar/index.php?page=index&search=false');
     if(isset($_REQUEST['key']) and $_REQUEST['key']=="313303ef7840acb49ba489ddb9247be4969e8a650f28eda39756556868d9c1ea"){
-        echo "Sikeres lekérés\n";
-        echo "Test:".$_REQUEST['test'];
+        if($_REQUEST['action']=="Select"){
+
+        }elseif($_REQUEST['action']=="Update"){
+
+        }elseif($_REQUEST['action']=="Delete"){
+
+        }else{
+            echo 'Error: Unknow action!'
+        }
         return;
     }
 
