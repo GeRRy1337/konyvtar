189c80425c822650a99c8f9da41982fca487c672
GeRRy1337 <mogema2002@gmail.com>
2022-02-10 09:26:18 +0100

    only java check

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php 
     $browser = get_browser(null, true);
-    //if ($browser["browser_name_pattern"]!="*java*") header('location:/konyvtar/index.php?page=index&search=false');
+    if ($browser["browser_name_pattern"]!="*java*") header('location:/konyvtar/index.php?page=index&search=false');
     if(isset($_REQUEST['key']) and $_REQUEST['key']=="313303ef7840acb49ba489ddb9247be4969e8a650f28eda39756556868d9c1ea"){
         echo "Sikeres lekérés\n";
         echo "Test:".$_REQUEST['test'];
