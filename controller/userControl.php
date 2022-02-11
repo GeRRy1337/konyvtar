db06c0a7f7c792f03bf3045c777fba482011e9e6
Gergő <mogema2002@gmail.com>
2022-02-11 20:30:28 +0100

    register secure

--- controller/userControl.php
+++ controller/userControl.php
@@ -8,17 +8,30 @@ if(isset($_POST['user']) and isset($_POST['pw'])) {
 			$loginError .= "Hibás email!<br>";
 		}
 		if($loginError == '') {
-			$sql = "SELECT id FROM users WHERE username = '".$_POST['user']."' ";
-			if(!$result = $conn->query($sql)) echo $conn->error;
-			
-			if ($result->num_rows > 0) {
-				$loginError .= "Ez a felhasználónév már foglalt<br>";
-			}else{
-				$sql = "INSERT INTO users(username,password,email) VALUES('".$_POST['user']."','".md5($_POST['pw'])."','".$_POST['email']."') ";
-				if(!$result = $conn->query($sql)) {
-					echo "Error: " . $sql . "<br>" . $conn->error;
+			$sql = "SELECT id FROM users WHERE username = ? ";
+			if($stmt = $conn->prepare($sql)){
+				$stmt->bind_param("s", $us);
+				$us = $_POST['user'];
+				$stmt->execute();
+				if(!$result = $stmt->get_result()) echo "SQL Error";
+				
+				if ($result->num_rows > 0) {
+					$loginError .= "Ez a felhasználónév már foglalt<br>";
 				}else{
-					header('Location: index.php?page=userControl');
+					$sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+			
+					if($stmt = $conn->prepare($sql)){
+						$stmt->bind_param("sss", $us,$pw,$em);
+						$us = $_POST['user'];
+						$pw = md5($_POST['pw']);
+						$em = $_POST['email'];
+						if($stmt->execute()){
+							header('Location: index.php?page=userControl');
+						} else{
+							echo "SQL Error";
+						}
+						$stmt->close();
+					}
 				}
 			}
 		}
