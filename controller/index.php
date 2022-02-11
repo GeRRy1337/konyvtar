59e12bbc6d32578cbdc020427a1ef1c0700ba676
Gergő <mogema2002@gmail.com>
2022-02-11 19:44:45 +0100

    refresh

--- controller/index.php
+++ controller/index.php
@@ -2,16 +2,20 @@
     if(!empty($_POST['search'])){
         $_SESSION['indexPage']=1;
         $_SESSION['search']=$_POST['search'];
+        header("Location: index.php?page=index");
     }
     if(isset($_POST['switchPage'])){
         $_SESSION['indexPage']=$_POST['switchPage'];
+        header("Location: index.php?page=index");
     }elseif(isset($_POST['forward'])){
         if( $_SESSION['indexPage']<$BookVar->getMax($conn)){
             $_SESSION['indexPage']++;
+            header("Location: index.php?page=index");
         }
     }elseif(isset($_POST['backward'])){
         if($_SESSION['indexPage']>1){
             $_SESSION['indexPage']--;
+            header("Location: index.php?page=index");
         }
     }
     $bookIds=$BookVar->bookList($conn);
