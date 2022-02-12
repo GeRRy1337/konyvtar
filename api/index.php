0ba57a641c5e7c136a8d6fa0c8fb95684b10cef6
Gergő <mogema2002@gmail.com>
2022-02-12 09:25:25 +0100

    java check fix

--- api/index.php
+++ api/index.php
@@ -1,8 +1,8 @@
 <?php 
-    $browser = get_browser(null, true);
-    if ($browser["browser_name_pattern"]!="*java*") header('location:/konyvtar/index.php?page=index&search=false');
+    $browser = $_SERVER['HTTP_USER_AGENT'];
+    if (strpos($browser,"Java")==false) header('location:/gergo/szakdolgozat/index.php?page=index&search=false');
     if(isset($_REQUEST['key']) and $_REQUEST['key']=="313303ef7840acb49ba489ddb9247be4969e8a650f28eda39756556868d9c1ea"){
-        require (realpath($_SERVER["DOCUMENT_ROOT"]).'/konyvtar/Includes/db.inc.php');
+        require (realpath($_SERVER["DOCUMENT_ROOT"]).'/gergo/szakdolgozat/Includes/db.inc.php');
         if($_REQUEST['action']=="Select"){
             $condition="";
             if(isset($_REQUEST["from"])){
