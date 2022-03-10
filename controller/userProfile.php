c1011a802b2bcbee787e20b48099780329eba85f
GeRRy1337 <mogema2002@gmail.com>
2022-03-10 12:03:44 +0100

    user profile
    sidebar,
    change password

--- controller/userProfile.php
+++ controller/userProfile.php
@@ -1,4 +1,29 @@
 <?php
     if(!isset($_SESSION['id'])) header('Location:index.php?page=index');
+    $loginError="";
+    if(isset($_POST['oldPw']) and isset($_POST['pw']) and isset($_POST['pw2'])){
+        if($_POST['pw2']!=$_POST['pw']) $loginError.=$langArr["passwordMatch"]."<br>";
+        $sql = "SELECT id FROM users WHERE password = ? ";
+        if($stmt = $conn->prepare($sql)){
+            $stmt->bind_param("s", $pw);
+            $pw = md5($_POST['pw']);
+            $stmt->execute();
+            if(!$result = $stmt->get_result()) echo "SQL Error";
+            if ($result->num_rows > 0) {
+                $loginError .= $langArr['passError']."<br>";
+            }else{
+                if($loginError == '') {
+                    $sql = "UPDATE users SET users.password=? WHERE id=?";
+                    if($stmt = $conn->prepare($sql)){
+                        $stmt->bind_param("si", $pw,$id);
+                        $pw = md5($_POST['pw']);
+                        $id= $_SESSION['id'];
+                        $stmt->execute();
+                        $stmt->close();
+                    }
+                }
+            }
+        }
+    }
     include 'view/userProfile.php';
 ?>
--- view/userProfile.php
+++ view/userProfile.php
@@ -1,46 +1,98 @@
-<div class="container">
-    <?php
-    $user->set_user($_SESSION['id'],$conn);
-    $userBorrowedList=$user->userBorrowed($conn);
-    if(sizeof($userBorrowedList)>0){
-    ?>
-    <div class="row justify-content-center">
-        <div class="col-12">
-        <h3><?php echo $langArr['borrow'];?></h3>
-        <table class="table table-dark table-bordered w-100">
-            <thead class="thead-light">
-                <tr>
-                    <th>#</th>
-                    <th><?php echo $langArr['bookTitle'];?></th>
-                    <th><?php echo $langArr['bookNum'];?></th>
-                    <th><?php echo $langArr['bookDate'];?></th>
-                </tr>
-            </thead>
-            <tbody>
-        <?php
 
-            $index=1;
-            foreach($userBorrowedList as $book){
-                echo '<tr>';
-                    echo '<td>'.($index++).'</td>';
-                    foreach($book as $element){
-                        echo '<td>'.$element.'</td>';
-                    }
-                echo '</tr>';
-            }
-        ?>
-            </tbody>
-        </table>
-        </div>
+<div class="row">
+    <div class="col-2">
+        <nav class="nav navbar-light sidebar">
+            <ul class="navbar-nav mr-auto ">
+                <div>Menü</div>
+                <li class="nav-item"><button class="sidebar-item" onclick='setDisplay("Settings")'>Beállítások</button></li>
+                <li class="nav-item"><button class="sidebar-item" onclick='setDisplay("Borrow")'>Kölcsönzések</button></li>
+            </ul>
+        </nav>
     </div>
-    <?php 
-    }else{
-    ?>
+    <div class="container">
+        <?php
+        $user->set_user($_SESSION['id'],$conn);
+        ?>
+        <div class="col" id="Settings">
+            <form method="post" id="passForm">
+                <h1>Új jelszó</h1>
+                Jelenlegi jelszó: <input type="password" name="oldPw" required><br>
+                Új jelszó: <input type="password" name="pw" id="pw" required><br>
+                Új jelszó megerősítése: <input type="password" name="pw2" id="pw2" required><br>
+                <input type="submit">
+                <p id="error"></p>
+                <?= $loginError ?>
+            </form>
+        </div>
+        <?php
+        $userBorrowedList=$user->userBorrowed($conn);
+        if(sizeof($userBorrowedList)>0){
+        ?>
+        <div id="Borrow" class="col justify-content-center">
+            <h3><?php echo $langArr['borrow'];?></h3>
+            <table class="table table-dark table-bordered w-100">
+                <thead class="thead-light">
+                    <tr>
+                        <th>#</th>
+                        <th><?php echo $langArr['bookTitle'];?></th>
+                        <th><?php echo $langArr['bookNum'];?></th>
+                        <th><?php echo $langArr['bookDate'];?></th>
+                    </tr>
+                </thead>
+                <tbody>
+            <?php
 
-    <p><?php echo $langArr['noBorrow'];?></p>
+                $index=1;
+                foreach($userBorrowedList as $book){
+                    echo '<tr>';
+                        echo '<td>'.($index++).'</td>';
+                        foreach($book as $element){
+                            echo '<td>'.$element.'</td>';
+                        }
+                    echo '</tr>';
+                }
+            ?>
+                </tbody>
+            </table>
+            
+        </div>
+        <?php 
+        }else{
+        ?>
 
-    <?php
-    }
-    ?>
-    
+        <p><?php echo $langArr['noBorrow'];?></p>
+
+        <?php
+        }
+        ?>
+    </div>
 </div>
+
+<script>
+    document.onload=setDisplay("Settings");
+    function setDisplay(disId){
+        displayNone();
+        document.getElementById(disId).style.display = "block";
+    }
+    function displayNone(){
+        document.getElementById("Settings").style.display = "none";
+        document.getElementById("Borrow").style.display = "none";
+    }
+    document.getElementById('pw2').oninput=document.getElementById('pw').oninput=function (){
+        if( document.getElementById('pw').value != document.getElementById('pw2').value ){
+            document.getElementById('error').innerHTML="<?php echo $langArr['passwordMatch'];?>";
+        }else if(document.getElementById('pw').value.includes("asd") || document.getElementById('pw2').value.includes("asd")){
+            document.getElementById('error').innerHTML="<?php echo $langArr['weakPassword'];?>";
+        }else{
+            document.getElementById('error').innerHTML="";
+        }
+    };
+    document.getElementById('passForm').onsubmit = function() {
+        if( document.getElementById('pw').value != document.getElementById('pw2').value ){
+            return false;
+        }
+
+        return true;
+    }
+    
+</script>
